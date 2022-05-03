b8569b2d47dc9f6ec5e7de4aebff8889294ae4b1
Nouman <noumancodes@gmail.com>
2022-05-03 15:48:58 -0400

    Formatted js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="bg-black w-screen h-screen flex items-center justify-center">
-      <p className="text-white text-lg">
-        Coming soon ...
-      </p>
+      <p className="text-white text-lg">Coming soon ...</p>
     </div>
   );
 }

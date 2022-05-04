8dca2a3c11ea4d069494aecc479947eca500f09d
Nouman <noumancodes@gmail.com>
2022-05-03 20:58:53 -0400

    Created Folder for the App Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="bg-black w-screen h-screen flex items-center justify-center">
       <p className="text-white text-lg">Coming soon ...</p>
     </div>
   );
-}
+};
 
-export default App;
+export { App as default };

f25f6b6e8a48018678144b84deaf69d10682daa1
Nouman <noumancodes@gmail.com>
2022-05-04 21:40:03 -0400

    Made projects look good on mobile and desktop

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,12 +5,22 @@ const Projects = () => {
     return (
         <div
             id="projects-tree"
-            className="bg-[#1E1E1E] w-screen h-screen flex flex-col items-center gap-1 relative"
+            className="bg-[#1E1E1E] w-screen h-screen flex flex-col items-center gap-1"
         >
             <p className="bg-[#F3A946] mt-1 p-4 border-solid border-2 border-[#1E1E1E] rounded-xl text-2xl">
-                Projects Tree
+                Project Tree
             </p>
-            <img src={tree} alt="Project Tree" className="w-[210px]" />
+            <div className="relative w-[446px] flex justify-center">
+                <img
+                    src={tree}
+                    alt="Project Tree"
+                    className="w-[105px] sm:w-[210px]"
+                />
+                {/* Projects here */}
+                <div className="absolute border-solid border-[#f3a946] border-2 rounded-xl p-2 top-4 right-10 sm:top-14 sm:-right-4">
+                    <p className="text-[#F3A946]">Coming Soon</p>
+                </div>
+            </div>
         </div>
     );
 };
